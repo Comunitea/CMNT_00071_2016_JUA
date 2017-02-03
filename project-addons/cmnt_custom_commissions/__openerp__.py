0733caaac7712612cafa448df6f6d97fb915d248
javierjcf <javierjcf@gmail.com>
2017-02-03 13:44:26 +0100

    [ADD]Añadido módulo para personalización comisiones

---
+++ project-addons/cmnt_custom_commissions/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Custom commissions CMNT',
+    'version': '8.0.0.0.0',
+    'author': 'Comunitea ',
+    "category": "CRM",
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'contributors': [
+        "Comunitea ",
+        "Javier Colmenero <javier@comunitea.com>"
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+    'test': [
+    ],
+    "installable": True
+}
